ba35111ee894ff9b4bd8b1270240f9ff71009be6
Vinicius T. Ferreira <viniciustferreira@vtf.br>
2019-06-17 11:40:53 -0300

    Change tester class and set the @Test annotation.

--- test/AccountTester.java
+++ test/AccountTester.java
@@ -3,4 +3,22 @@ import static org.junit.Assert.*;
 
 public class AccountTester {
 
+    private SavingsAccount sacc;
+
+    @Before
+    protected void setUp() throws Exception {
+        sacc = new SavingsAccount();
+    }
+
+// The garbage collector does this implicitly, so this method is here just for didactic reason.
+    @After
+    protected void tearDown() {
+        sacc = null;
+    }
+
+    @Test
+    public void testTransferIfEnoughFunds () throws InsufficientFundsException {
+        
+    }
+
 } 
